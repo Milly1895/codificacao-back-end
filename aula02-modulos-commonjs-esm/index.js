0ddf5540641d199ca6012e32e5197429001305f3
jamily_silva <jamilysilva3614@gmail.com>
2026-09-11 16:55:05 -0300

    feat: adiciona registro de inicialização do servidor

---
+++ aula02-modulos-commonjs-esm/index.js
@@ -0,0 +1,23 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { formatLog } from './utils.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+async function salvarLogSystem(mensagemLog) {
+   try{
+    const pastaLog = path.join(__dirname, 'logs');
+    const arquivoLog =path.join(pastaLog, 'system.log');
+    await fs.mkdir(pastaLog, {recursive:true});
+    const registroLog =formatLog(mensagemLog);
+    await fs.appendFile(arquivoLog, registroLog, 'utf-8');
+
+    console.log('Log Registrado com sucesso!');
+   }catch(erro){
+    console.error('Erro ao Registrar Log:', erro);
+   }
+}
+salvarLogSystem('Inicialixação do servidor concluída!\n');
+salvarLogSystem('Conexão com o banco de dados estabelecidoa!\n');
